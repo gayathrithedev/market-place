3191a33981097ca6387084a77fad2edd53637b8c
Gayathri Perumal <gayathriperumal100@gmail.com>
2024-06-06 12:57:36 +0530

    Add react navigation support

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faLocationDot} from '@fortawesome/free-solid-svg-icons/faLocationDot';
 function App() {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Welcome</Text>
-        <FontAwesomeIcon icon={faLocationDot} />
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView>
+        <View>
+          <Text>Welcome</Text>
+          <FontAwesomeIcon icon={faLocationDot} />
+        </View>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 }
 
